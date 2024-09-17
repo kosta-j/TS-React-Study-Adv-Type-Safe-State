454bfd2a204a436b67e3f72e2487dcb0c5ca80c1
Konstantin Zhukov <zhukov.konst.sl@gmail.com>
2024-09-17 23:35:07 +0200

    add stop/start timers feature

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,15 +1,31 @@
-import { useEffect, useState } from "react";
-import { type Timer as TimerProps } from "../store/timers-context.tsx";
+import { useEffect, useRef, useState } from "react";
+import {
+  useTimersContext,
+  type Timer as TimerProps,
+} from "../store/timers-context.tsx";
 import Container from "./UI/Container.tsx";
 
 export default function Timer({ name, duration }: TimerProps) {
-  useEffect(() => {
-    setInterval(function () {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
-  }, []);
-
+  const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(function () {
+        setRemainingTime((prevTime) => prevTime - 50);
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
